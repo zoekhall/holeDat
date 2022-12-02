ad5b157d9b46439b808cff6344a70e34a69691c5
codingMustache <jorcar1986@gmail.com>
2022-12-02 13:37:19 -0600

    Tested & Working: react-bootstrap

--- client/src/components/app.tsx
+++ client/src/components/app.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
-
-import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 const App = () => (
      <>
-          <Button variant="outline-primary">Primary</Button>{' '}
-          <Button variant="outline-secondary">Secondary</Button>{' '}
-          <Button variant="outline-success">Success</Button>{' '}
-          <Button variant="outline-warning">Warning</Button>{' '}
-          <Button variant="outline-danger">Danger</Button>{' '}
-          <Button variant="outline-info">Info</Button>{' '}
-          <Button variant="outline-light">Light</Button>{' '}
-          <Button variant="outline-dark">Dark</Button>
+          <style type="text/css">
+               {`
+                    .btn-flat {
+                         background-color: purple;
+                         color: white;
+                    }
+
+                    .btn-lg {
+                         padding: 1rem 1.5rem;
+                         font-size: 1.5rem;
+                    }
+               `}
+          </style>
+          <Button variant="flat" size="lg">
+               flat button
+          </Button>
      </>
 )
 

e89f70a15f8c98f141b25783331a27a741829f71
codingMustache <jorcar1986@gmail.com>
2022-12-02 00:04:53 -0600

    Got front-end working

---
+++ server/db/index.ts
@@ -0,0 +1,1 @@
+// make db server here
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,24 @@
+import express, { Request, Response } from 'express';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
 
-console.log('test')
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve('client', 'dist')));
+app.use(express.json());
+
+const PORT = process.env.PORT || 5555
+
+app.get('/api', (req: Request, res: Response) => {
+     console.log('test')
 
 
+})
 
+app.listen(PORT, () => {
+     console.log(`listening at http://localhost:${PORT}`);
+});

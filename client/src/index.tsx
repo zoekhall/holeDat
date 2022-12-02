057fe49dbf4b2246462587360206597e1a364072
codingMustache <jorcar1986@gmail.com>
2022-12-01 21:13:06 -0600

    setup branch

---
+++ client/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+
+// components import
+import App from './components/app'
+
+
+const rootElement = document.getElementById("app");
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(rootElement);
+
+
+root.render(<App />)

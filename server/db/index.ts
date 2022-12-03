d4c73ca5f5d7118587597b84fcca94aaf344d2b3
codingMustache <jorcar1986@gmail.com>
2022-12-02 20:03:46 -0600

    Setup: Squelize

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,1 +1,18 @@
 // make db server here
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize('codeforgeek', 'postgres', 'shahid', {
+     host: 'localhost',
+     dialect: 'postgres',
+     pool: {
+          max: 9,
+          min: 0,
+          idle: 10000
+     }
+});  
+
+sequelize.authenticate().then(() => {
+     console.log("Success!");
+}).catch((err) => {
+     console.log(err);
+});

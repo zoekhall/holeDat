8826678b493e52c3c3b2f8cf4ab5c2e6c2dbf5d0
zoekhall <zoe.k.hall@gmail.com>
2022-12-06 19:56:49 -0600

    navigation bar functioning

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -1,30 +1,36 @@
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
 
 const NavBar = () => {
   const menuItems = ['Map', 'About', 'Feed']
   //Check if user is logged in to change text from login to logout
-  return (
+  return ( 
     <>
-      <nav>
-        <ul>
-          {menuItems.map((item, i) => (
-            <li key={i} >
-              <Link to={`/${item}`} >
-                {item}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <a href='/'><h1>🕳️HoleDat</h1></a>
-        {/* add profile pic*/}
-        <a href='/auth/google/callback'>
-          <button>Login with Google</button>
-        </a>
-      </nav>
-      <Outlet />
-    </>
-  )
-};
+      <Navbar bg="light" expand="lg">
+      <Container>
+        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto"> 
+              {menuItems.map((item, i) => (
+                <Nav.Link key={i} href={`/${item}`}>
+                    {item}
+                </Nav.Link>  
+              ))} 
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+        <Navbar.Brand href='/'>🕳️HoleDat</Navbar.Brand>
+        <Button href='/auth/google/callback' variant="outline-primary">Sign In</Button>{' '}
+      </Navbar>
+  <Outlet /> 
+    </> 
+  );
+}
 
 export default NavBar;

f1c52dee65ee7367e8f578e62cb18a2b9d29f4cb
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2022-12-09 15:39:01 -0600

    feed dynamicly rendering datbase pothole images

--- client/components/feed/Feed.tsx
+++ client/components/feed/Feed.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import FeedEntry from './FeedEntry';
+
 
 function Feed() {
-  return <p>Feed</p>;
+
+  type phImg = {
+    image_id: number;
+    photoURL: string;
+    caption: string;
+    createdAt: string;
+    updatedAt: string;
+  };
+
+  const [globalFeed, setGlobalFeed] = useState<phImg[]>([]);
+
+  const getAllImgs = () => {
+    axios
+      .get('/api/imgs')
+      .then((data) => setGlobalFeed(data.data))
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(getAllImgs, [])
+  console.log(globalFeed)
+  return (
+    <div>
+      <h1>
+        Pothole Feed
+      </h1>
+      {globalFeed.map(imgVal => <FeedEntry key={imgVal.image_id} imgObj={imgVal} />)}
+    </div>
+  );
 }
 
 export default Feed;

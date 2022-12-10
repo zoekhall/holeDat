f474de89ebea8db9f9d6cfb1c120652cd181ce78
codingMustache <jorcar1986@gmail.com>
2022-12-10 16:48:24 -0600

    rendering the everything in the popup

---
+++ server/routes/location.routes.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config()
+const location = express.Router();
+const api_key = process.env.GOOGLE_GEOCODING
+
+
+
+location.get('/getAddy', (req: Request, res: Response) => {
+  const { lat, lon } = req.query
+  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&location_type=ROOFTOP&result_type=street_address`, { params: { key: api_key } })
+    .then(data=> console.log(data.data))
+    .catch(err=> console.log(err))
+  res.sendStatus(200)
+});
+
+export default location

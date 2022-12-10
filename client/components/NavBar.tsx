10e5741b494cd3f535a22e4b7e2c6ea229aee69b
zoekhall <zoe.k.hall@gmail.com>
2022-12-10 15:27:21 -0600

    fixing merge

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-
+import Outlet from './App'
 
 
 const NavBar = () => {

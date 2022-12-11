cba7222c920bbc48f988edda725e8153c595faed
zoekhall <zoe.k.hall@gmail.com>
2022-12-11 02:03:56 -0600

    formatting done working on map

--- client/components/addPothole/AddPothole.tsx
+++ client/components/addPothole/AddPothole.tsx
@@ -4,82 +4,76 @@ import Form from 'react-bootstrap/Form';
 import PotholeStatus from './PotholeStatus';
 import PotholeLocation from './PotholeLocation';
 import PotholePic from './PotholePic';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
 import PotholeRating from './PotholeRating';
-import axios from 'axios';
-
+// import axios from 'axios';
 
 function AddPothole() {
-
   //objs to be be sent to database
-  const potObj: { fixed: boolean, lat: number, lon: number } = {
+  const potObj: { fixed: boolean; lat: number; lon: number } = {
     fixed: false,
     lat: 0,
-    lon: 0
+    lon: 0,
   };
 
-  const imgObj: { photoURL: string, caption: string } = {
-    photoURL: '', 
-    caption: ''
-  }
-  
-  const ratingObj: { overall: number } = {
-    overall: 0
+  const imgObj: { photoURL: string; caption: string } = {
+    photoURL: '',
+    caption: '',
   };
-  
-  //updating objects with filled out information 
+
+  const ratingObj: { overall: number } = {
+    overall: 0,
+  };
+
+  //updating objects with filled out information
   const updatePotStatus = (newStatus: boolean) => {
     potObj.fixed = newStatus;
-  }
+  };
 
   const updateImage = (val: any, type: string) => {
-    imgObj[type] = val; 
-  }
+    imgObj[type] = val;
+  };
 
   const updateRating = (rating: number) => {
-    ratingObj.overall = rating; 
+    ratingObj.overall = rating;
     console.log(ratingObj);
-  }
+  };
+  //SUCCESSSSS
+  // //photo file func
+  //   const sendData = (file) => {
+  //     const formData = new FormData();
+  //     formData.append('file', file);
 
-  //photo file func
-    const sendData = (file) => {
-      const formData = new FormData();
-      formData.append('file', file);
-
-      if (formData) {
-        axios({
-          method: 'post',
-          url: '/api/imgs/addimg',
-          data: formData,
-        })
-          .then((data) => console.log(data))
-          .catch((err) => console.log(err));
-      }
-    };
+  //     if (formData) {
+  //       axios({
+  //         method: 'post',
+  //         url: '/api/imgs/addimg',
+  //         data: formData,
+  //       })
+  //         .then((data) => console.log(data))
+  //         .catch((err) => console.log(err));
+  //     }
+  //   };
 
   const handleSubmit = () => {
-      console.log(potObj, imgObj, ratingObj)
-      console.log('hey')
-      // sendData(imgObj.photoURL); //send data to cloud
-    }
+    console.log(potObj, imgObj, ratingObj);
+    console.log('hey');
+    // sendData(imgObj.photoURL); //send data to cloud
+  };
 
   return (
-    <div style={{ border: '1px solid lightgray', padding: '100px', backgroundColor: 'white' }}>
-      <Form>
-        <div id='PotholeQuestions'>
-          <h2>Report a Pothole</h2>
-          <br></br>
-          <PotholeLocation />
-          <PotholeStatus handleStatus={(newStatus) => updatePotStatus(newStatus)} />
-          <PotholePic handleImage={(val, type) => updateImage(val, type)} />
-          <PotholeRating handleRating={(rating) => updateRating(rating)} />
-        </div>
+    <Form id='addPothole'>
+        <h1>Report a Pothole</h1>
+        <br></br>
+        <PotholeLocation />
+        <PotholeStatus handleStatus={(newStatus) => updatePotStatus(newStatus)} />
+        <PotholePic handleImage={(val, type) => updateImage(val, type)} />
+        <PotholeRating handleRating={(rating) => updateRating(rating)} />
         <Button onClick={handleSubmit}>Test Submit Button</Button>
         <Button type='submit' variant='outlined-dark' onClick={handleSubmit}>
           Submit
         </Button>
-      </Form>
-    </div>
+    </Form>
   );
 }
 
--- client/components/addPothole/PotholeLocation.tsx
+++ client/components/addPothole/PotholeLocation.tsx
@@ -1,20 +1,43 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { AddressAutofill } from '@mapbox/search-js-react'
+import axios from 'axios';
+import { InputGroup } from 'react-bootstrap';
 
-const mapAPI = 'https://api.mapbox.com/geocoding/v5/{endpoint}/{search_text}.json';
-const mapbox_token = 'pk.eyJ1IjoiemFjaG1hcnVsbG8iLCJhIjoiY2xhazZ5aGxyMDQ3bzNwbzZ2Z3N0b3lpMyJ9.65G-mwqhbWFy77O_I0LkOg';
+const mapbox_token =
+  'pk.eyJ1IjoiemFjaG1hcnVsbG8iLCJhIjoiY2xhazZ5aGxyMDQ3bzNwbzZ2Z3N0b3lpMyJ9.65G-mwqhbWFy77O_I0LkOg';
 
 const PotholeLocation = () => {
+  const [location, setLocation] = useState<string>('');
+  // const [latitude, setLat] = useState<number>(0);
+  // const [longitude, setLongitude] = useState<number>(0);
 
-  const [latitude, setLat] = useState<number>(0)
-  const [longitude, setLongitude] = useState<number>(0);
-
-
+  const updateLatLon = () => {
+    const formattedLocation = location.split(' ').join('%20');
+    const mapAPI = `"https://api.mapbox.com/geocoding/v5/mapbox.places/${formattedLocation}.json?access_token=${mapbox_token}`;
+    axios
+      .get(mapAPI)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
 
   return (
-    <Form.Group className='mb-5' controlId='addPotLocation' autocomplete='address-line1'>
-      <Form.Label>What is the Approximate Location of the Pothole?</Form.Label>
-      <Form.Control placeholder='1234 Main St' required />
+    <Form.Group className='mb-5'>
+      <Form.Label>Where Dat Pothole At?</Form.Label>
+      <InputGroup id='addPotLocation'>
+        <AddressAutofill accessToken={mapbox_token}>
+          <Form.Control
+            id='mapfill'
+            name='address'
+            placeholder='Address'
+            type='text'
+            autoComplete='street-address'
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </AddressAutofill>
+        <Button variant='flat' onClick={updateLatLon}>Add Address</Button>
+      </InputGroup>
     </Form.Group>
   );
 };

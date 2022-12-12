c7d091fffe94aa9efc5a60ac8c14c2fe61442c02
codingMustache <jorcar1986@gmail.com>
2022-12-11 22:58:49 -0600

    made home page working

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from 'express';
 const user = express.Router();
+import { getRecentUsers, getUserData } from '../models/user.model';
 
-import { getUserData } from '../models/user.model'
-/*
+
 user.get('/', (req: Request, res: Response) => {
-  getRecentUser((data) => res.status(222).send(data));
+  getRecentUsers((data) => res.status(222).send(data));
 });
-*/
+
 
 user.get('/userAtId:id', (req: Request, res: Response) => {
   const { id } = req.params;

354f7b79b8d137e8a252e9fdcb8c06359ea5c2c9
zoekhall <zoe.k.hall@gmail.com>
2022-12-11 23:26:24 -0600

    cleaning

--- server/models/pothole.model.ts
+++ server/models/pothole.model.ts
@@ -6,8 +6,8 @@ export const getAllPotholes = (cb) => {
     .catch((err) => console.error(err));
 };
 
-export const postPothole = (cb) => {
-  Pothole.create({})
+export const postPothole = (cb, obj = {}) => {
+  Pothole.create(obj)
     .then(data => cb(data))
     .catch(err => console.error(err))
 }
--- server/routes/pothole.routes.ts
+++ server/routes/pothole.routes.ts
@@ -1,14 +1,14 @@
 import express, { Request, Response } from 'express';
 const pothole = express.Router();
 
-import getAllPothole from '../models/pothole.model';
+import getAllPothole, { postPothole } from '../models/pothole.model';
 
 pothole.get('/', (req: Request, res: Response) => {
   getAllPothole((data) => res.status(222).send(data));
 });
 
 pothole.post('/addPothole', (req: Request, res: Response) => {
-  console.log(req)
+  postPothole(data => console.log(req))
 });
 
 export default pothole;

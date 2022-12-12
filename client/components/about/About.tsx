8258c44a195557117394926af9663235247d45e9
zachmarullo <zachmarullo@gmail.com>
2022-12-12 09:52:39 -0600

    Chart Rendering

--- client/components/about/About.tsx
+++ client/components/about/About.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement } from 'chart.js';
+// import '../../../node_modules/chart.js/auto';
+import { Bar } from 'react-chartjs-2';
 
-function About() {
-  return (
-    <h1>About Hole Dat</h1>
-  )
-}
+ChartJS.register(CategoryScale, LinearScale, BarElement);
+
+const About = () => {
+  type chart = {
+    labels: string[];
+    datasets: { label: string; data: number[]; borderColor: string; backgroundColor: string }[];
+  };
+  const [chartData, setChartData] = useState<chart>({ labels: [], datasets: [] });
+
+  const [chartOptions, setChartOptions] = useState({});
+
+  useEffect(() => {
+    setChartData({
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+      datasets: [
+        {
+          label: 'Potholes added by day of the week',
+          data: [3, 5, 2, 14, 5, 2, 7],
+          borderColor: 'rgb(53, 162, 235)',
+          backgroundColor: 'rgba(53, 162, 235, 0.4)',
+        },
+      ],
+    });
+    setChartOptions({
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Potholes Added by Day of the Week',
+        },
+      },
+    });
+  }, []);
+
+  return <Bar options={chartOptions} data={chartData} />;
+};
 
 export default About;

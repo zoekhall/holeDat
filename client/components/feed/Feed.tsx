51e230287cf0cbe257f2fae23f5ede6620b1fb30
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2022-12-13 15:39:10 -0600

    sort by uniqe potholes working

--- client/components/feed/Feed.tsx
+++ client/components/feed/Feed.tsx
@@ -11,23 +11,34 @@ function Feed() {
     caption: string;
     createdAt: string;
     updatedAt: string;
+    potholePotholeId: number;
   };
 
   const [globalFeed, setGlobalFeed] = useState<phImg[]>([]);
 
-  const getAllImgs = () => {
+  const getAllImgs = () => { // gets all images of all potholes
     axios
       .get('/api/imgs')
       .then((data) => setGlobalFeed(data.data))
       .catch((err) => console.log(err));
   }
 
-  const sortByNew = () => {
+  const sortByNew = () => { // sorts the current filter by new
     let resultArr = globalFeed.sort((a, b) => {
-      return new Date(a.createdAt).valueOf() - new Date(b.createdAt).valueOf();
+      return new Date(a.createdAt).valueOf() - new Date(b.createdAt).valueOf(); //sort the array by the createdAt value
     })
     setGlobalFeed([...resultArr])
-    console.log(globalFeed)
+  }
+
+  const sortByUnique = () => {
+    let filtArr: number[] = [];
+    let resultArr = globalFeed.filter(val => {
+      if (!filtArr.includes(val.potholePotholeId)) { // if the potholePotholeId has not already been seen
+        filtArr.push(val.potholePotholeId)
+        return true
+      }
+    })
+    setGlobalFeed([...resultArr])
   }
 
 
@@ -35,10 +46,11 @@ function Feed() {
   useEffect(getAllImgs, [])
   return (
     <div>
+      <button onClick={getAllImgs}>Reset</button>
       <h1>
         Pothole Feed
       </h1>
-      Sort: <button onClick={sortByNew}>New</button>
+      Sort: <button onClick={sortByNew}>New</button> <button onClick={sortByUnique}>Unique</button>
       {globalFeed.map(imgVal => <FeedEntry key={imgVal.image_id} imgObj={imgVal} />)}
     </div>
   );

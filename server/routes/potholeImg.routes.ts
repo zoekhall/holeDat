d6b9d04f13705f19c386dea629dff0ac42498dfe
zachmarullo <zachmarullo@gmail.com>
2022-12-17 15:37:06 -0600

    Home Page Sliders Autoplay Added & Socket.io total number of potholes added

--- server/routes/potholeImg.routes.ts
+++ server/routes/potholeImg.routes.ts
@@ -81,11 +81,11 @@ imgs.get('/phstats', (req: Request, res: Response) => {
 });
 
 imgs.get('/atUser:id', (req: Request, res: Response) => {
-  const { id } = req.params
+  const { id } = req.params;
   if (!id) {
-    console.log(req.user, 'user')
+    console.log(req.user, 'user');
   }
-  getPotholeAtUserId(id, (data) => res.status(200).send(data))
-})
+  getPotholeAtUserId(id, (data) => res.status(200).send(data));
+});
 
 export default imgs;

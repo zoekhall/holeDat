317b54704d47e241dbdb32bc707719a575d7cef0
Samuel <samlittellmds@gmail.com>
2022-12-19 18:32:33 -0600

    comments deleting from the frontend and the db

--- server/routes/comments.routes.ts
+++ server/routes/comments.routes.ts
@@ -1,14 +1,13 @@
 import express, { Request, Response } from "express";
 const comments = express.Router()
-import { postComment, getComments } from "../models/comments.model";
+import { postComment, getComments, deleteCom } from "../models/comments.model";
 
 
 comments.post('/add', (req: Request, res: Response) => {
   const text = req.body[0]
   const phId = req.body[1]
   const user = req.user
-  postComment(text, phId, user)
-  res.sendStatus(200)
+  postComment(text, phId, user, data => res.status(200).send(data))
 })
 
 comments.get('/perPh', (req: Request, res: Response) => {
@@ -16,5 +15,15 @@ comments.get('/perPh', (req: Request, res: Response) => {
   getComments(potholeId, (data) => res.status(200).send(data.sort((a, b) => a.createdAt - b.createdAt)))
 })
 
+comments.delete('/', (req: Request, res: Response) => {
+  const { id } = req.query
+  deleteCom(id, (data)=> {
+    if (data) {
+      res.sendStatus(200)
+    } else {
+      res.sendStatus(404)
+    }
+  })
+})
 
 export default comments;

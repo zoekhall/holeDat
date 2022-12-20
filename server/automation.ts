f09bdea049c7035cd81acdf44109550a7885ccf8
codingMustache <jorcar1986@gmail.com>
2022-12-20 13:55:32 -0600

    cleaned up automation for badges

--- server/automation.ts
+++ server/automation.ts
@@ -7,23 +7,23 @@ export const issueBadges = async () =>{
   getTopThree(data=> {
     data.forEach(each => {
       //issue plat
-      if(each.count > 25){
+      if(each.count >= 20){
         User.update(
           {badge_id: 1},
           {where: {user_id: each.user_id}}
         )
           //issue Gold
-       } else if(each.count > 20){
+       } else if(each.count >= 15){
         User.update(
           {badge_id: 2},
           {where: {user_id: each.user_id}}
         )
-       } else if(each.count > 15){
+       } else if(each.count >= 10){
         User.update(
           {badge_id: 3},
           {where: {user_id: each.user_id}}
         )
-       } else if(each.count > 10){
+       } else if(each.count >= 5){
         User.update(
           {badge_id: 4},
           {where: {user_id: each.user_id}}
@@ -38,4 +38,4 @@ export const issueBadges = async () =>{
   })
 }
 
-setInterval(issueBadges, 1000 * 60 * 15)
+setInterval(issueBadges, 1000 * 60 * 15 )

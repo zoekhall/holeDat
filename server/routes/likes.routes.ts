a24272f336e29af40ce8978e2d90772eaeb6dcc9
codingMustache <jorcar1986@gmail.com>
2022-12-20 20:19:37 -0600

    cleaned up

--- server/routes/likes.routes.ts
+++ server/routes/likes.routes.ts
@@ -2,7 +2,6 @@ import express, { Request, Response } from 'express';
 const likes = express.Router();
 import { addLike } from '../models/like.model'
 likes.use('/', (req: Request, res: Response)=>{
-
   const {bol, userId_user, image_id} = req.query
   addLike(bol, userId_user, image_id)
   res.sendStatus(200)

85b2e03b94d18c17674523750a85201df6679d43
codingMustache <jorcar1986@gmail.com>
2022-12-20 20:20:26 -0600

    Co-authored-by: Sam Littell <samlittellmds@gmail.com> Co-authored-by: Zach Marullo <zachmarullo@gmail.com> Co-authored-by: sHolmes <sidneyselassie1@gmail.com>

--- server/routes/likes.routes.ts
+++ server/routes/likes.routes.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 const likes = express.Router();
+
 import { addLike } from '../models/like.model'
 likes.use('/', (req: Request, res: Response)=>{
   const {bol, userId_user, image_id} = req.query

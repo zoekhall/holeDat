26ae9097281c4c83155b1e65ddc7bb7609508e22
codingMustache <jorcar1986@gmail.com>
2022-12-21 20:35:13 -0600

    UpCone and DownCone working

--- client/components/pothole/Likes.tsx
+++ client/components/pothole/Likes.tsx
@@ -1,22 +1,87 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Likes = ({ user, image }: any) => {
 
+  const [ratio, setRatio] = useState<number>(0)
+  const [usersLike, setUsersLike] = useState<boolean | null>(null)
+  const [upConeStyle, setUpConeStyle] = useState<{ [key: string]: string }>({})
+  const [downConeStyle, setDownConeStyle] = useState<{ [key: string]: string }>({})
+
+  const { image_id } = image
+  const { userId_user } = user
+
   const sendLike = (bol: boolean) => {
-    const { userId_user } = user
-    const { image_id } = image
-    console.log(image)
-    axios.get('/api/likes/', { params: { bol, userId_user, image_id } })
-      .then(({ data }) => console.log(data))
+    axios.post('/api/likes/', { params: { bol, userId_user, image_id } })
+      .then(() => {
+        if (!bol && usersLike && usersLike !== null) {
+          setUsersLike(bol) /// ^ V --2
+          setRatio(ratio - 2)
+        } else if (bol && !usersLike && usersLike !== null) {
+          setUsersLike(bol) /// V ^ ++2
+          setRatio(ratio + 2)
+        } else if (bol && usersLike && usersLike !== null) {
+          setUsersLike(null) /// ^ ^
+          setRatio(ratio - 1)
+        } else if (!bol && !usersLike && usersLike !== null) {
+          setUsersLike(null) // V V +1
+          setRatio(ratio + 1)
+        } else if (usersLike === null && bol) {
+          setUsersLike(bol) // - ^ +1
+          setRatio(ratio + 1)
+        } else if (usersLike === null && !bol) {
+          setUsersLike(bol) // - v -1
+          setRatio(ratio - 1)
+        }
+      })
       .catch(err => console.log(err))
   }
 
-  console.log(user, image)
+  const getLikes = () => {
+    axios.get('/api/likes/', { params: { image_id } })
+      .then(({ data }) => {
+        const newRatio = data.upCone - data.downCone
+        setRatio(newRatio)
+      })
+      .catch(err => console.log(err))
+  }
+
+  const getUserLike = () => {
+    axios.get('/api/likes/user', { params: { userId_user, image_id } })
+      .then(({ data }) => {
+        if (data !== 'null') {
+          setUsersLike(data)
+        } else {
+          setUsersLike(null)
+        }
+      })
+  }
+
+  const handleColor = () => {
+    if (usersLike === null) {
+      setDownConeStyle({ rotate: '180deg', filter: 'grayscale(100%)' })
+      setUpConeStyle({ filter: 'grayscale(100%)' })
+    } else if (usersLike) {
+      setDownConeStyle({ rotate: '180deg', filter: 'grayscale(100%)' })
+      setUpConeStyle({ filter: 'grayscale(0%)' })
+    } else if (!usersLike) {
+      setDownConeStyle({ rotate: '180deg', filter: 'grayscale(0%)' })
+      setUpConeStyle({ filter: 'grayscale(100%)' })
+    }
+  }
+
+  useEffect(() => {
+    getUserLike()
+    getLikes()
+  }, [])
+  
+  useEffect(handleColor, [usersLike])
+
   return (
     <div className='like_pics'>
-      <img onClick={() => sendLike(true)} src='https://www.pinclipart.com/picdir/big/397-3976285_traffic-cone-png-clip-art-transparent-png.png' alt='up' width={20} />
-      <img onClick={() => sendLike(false)} src='https://www.pinclipart.com/picdir/big/397-3976285_traffic-cone-png-clip-art-transparent-png.png' alt='up' style={{ 'rotate': '180deg' }} width={20} />
+      <img onClick={() => sendLike(true)} src='https://www.pinclipart.com/picdir/big/397-3976285_traffic-cone-png-clip-art-transparent-png.png' alt='up' width={20} style={upConeStyle} />
+      {ratio}
+      <img onClick={() => sendLike(false)} src='https://www.pinclipart.com/picdir/big/397-3976285_traffic-cone-png-clip-art-transparent-png.png' alt='up' style={downConeStyle} width={20} />
     </div>
   )
 }
--- server/models/like.model.ts
+++ server/models/like.model.ts
@@ -1,18 +1,34 @@
 import Like from "../db/schema/likes.schema";
 
 
-export const addLike = (likeType, user_id, image_id) => {
-   Like.findAll({where: {user_id, image_id}})
+export const addLike = (newlikeType, user_id, image_id) => {
+   Like.findOne({where: {user_id, image_id}})
    .then(data=> {
-    if(!data[0]){
-      Like.create({likeType, user_id, image_id})
+    if(!data?.dataValues){
+      Like.create({likeType: newlikeType, user_id, image_id})
       .catch(err=> console.log(err))
     } else{
-      const { likes_id } = data[0].dataValues
-      Like.update({likeType},{where:{likes_id}})
-      .catch(err=> console.log(err))
+      const { likes_id, likeType } = data.dataValues
+      if(likeType === newlikeType){
+        Like.destroy({where:{likes_id}})
+        .catch(err=> console.log(err))
+      } else {
+        Like.update({likeType},{where:{likes_id}})
+        .catch(err=> console.log(err))
+      }
     }
   })
   .catch(err=> console.log(err))
+}
 
+export const getLikesByImgId = (image_id, cb) => {
+  Like.findAll({where: {image_id}})
+  .then(data => cb(data))
+  .catch(err => cb(err))
+}
+
+export const getLikeStatus = (user_id, image_id, cb) => {
+  Like.findOne({where: {user_id, image_id}})
+  .then(data=> cb(data))
+  .catch(err => console.log(err))
 }

b7630bf6f5087884eab7755542ae7a06f39e8b6b
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2022-12-23 10:08:17 -0600

    feed rendering user badge

--- server/routes/pothole.routes.ts
+++ server/routes/pothole.routes.ts
@@ -4,16 +4,16 @@ const pothole = express.Router();
 import getAllPothole, { findAndAddPothole, getMatchingPotholes } from '../models/pothole.model';
 
 pothole.get('/', (req: Request, res: Response) => {
-  getAllPothole(data=> res.status(200).send(data));
+  getAllPothole(data => res.status(200).send(data));
 });
 
 pothole.post('/findPothole', (req: Request, res: Response) => {
   getMatchingPotholes(data => res.status(200).send(data)
-  , req.body);
+    , req.body);
 });
 
 pothole.post('/addPothole', (req: Request, res: Response) => {
-  findAndAddPothole((data, status) => { 
+  findAndAddPothole((data, status) => {
     const rtnObj = { data, status }
     res.status(201).send(rtnObj)
   }, req.body)

6dcfa4765eaf0f610c131784d385f8c28a938498
codingMustache <jorcar1986@gmail.com>
2022-12-24 14:30:40 -0600

    Co-authored-by: sholm <SIDNEYHOLMES@users.noreply.github.com>

--- client/components/feed/FeedEntry.tsx
+++ client/components/feed/FeedEntry.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const FeedEntry = ({ imgObj }) => {
+const FeedEntry = ({ imgObj }: any) => {
 
   type badgeObj = {
     imgUrl: string;
@@ -11,31 +11,17 @@ const FeedEntry = ({ imgObj }) => {
   }
 
   const [addy, setAddy] = useState('')
-  const [userPhoto, setUserPhoto] = useState('')
   const [badge, setBadge] = useState<badgeObj>()
   const [hoverBool, setHoverBool] = useState<boolean>(false)
 
-  const getAddress = () => {
-    const { lat, lon } = imgObj.addressDetails
-
-    const getBadgeData = (userData) => {
-      if (userData !== 0) {
-        axios.get('/api/badges/getBadge' + userData)
-          .then(data => setBadge(data.data))
-          .catch(err => console.log(err));
-      }
-    }
-
-
-    axios.get('/api/location/getAddy', { params: { lat, lon } }) // on every image object get location
-      .then(data => setAddy(data.data))
+  const getInfo = () => {
+    console.log(imgObj.badge_id, 'TESTING')
+    axios.get('/api/badges/getBadge', { params: { badgeId: imgObj.badge_id } })
+      .then(({ data }) => setBadge(data))
       .catch(err => console.log(err));
 
-    axios.get('/api/user/UserAtId' + imgObj.user_id)
-      .then(data => {
-        setUserPhoto(data.data.photo)
-        getBadgeData(data.data.badge_id)
-      })
+    axios.get('/api/location/getAddy', { params: { lat: imgObj.lat, lon: imgObj.lon } }) // on every image object get location
+      .then(({ data }) => setAddy(data.split(',')[0]))
       .catch(err => console.log(err));
   }
 
@@ -43,10 +29,10 @@ const FeedEntry = ({ imgObj }) => {
     setHoverBool(option)
   }
 
-  useEffect(getAddress, [])
+  useEffect(getInfo, [])
   return (
     <div>
-      <h3>{addy.split(',')[0]}</h3>
+      <h3>{addy}</h3 >
       <Link to={`/Pothole:${imgObj.pothole_id}`}>
         <img
           style={{ borderRadius: '18px' }}
@@ -56,10 +42,9 @@ const FeedEntry = ({ imgObj }) => {
           height='50%'
         />
       </Link>
-      <img src={userPhoto} alt='Image' width="10%"></img>
+      <img src={imgObj.photo} alt='Image' width="10%"></img>
       <img onMouseOver={() => onBadgeHover(true)} onMouseOut={() => onBadgeHover(false)} src={badge?.imgUrl} alt='Image' width="10%"></img>
       {hoverBool ? badge?.description : badge?.name}
-
     </div>
   );
 };

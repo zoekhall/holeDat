2f1fa36e5abf6df1fb08e1aaebec233b16c00b03
zoekhall <zoe.k.hall@gmail.com>
2023-01-04 17:39:59 -0600

    pseudocoded potholelocation

--- client/components/addPothole/formQuestions/PotholeLocation.tsx
+++ client/components/addPothole/formQuestions/PotholeLocation.tsx
@@ -13,26 +13,29 @@ const mapToken =
 //Componenent with the location search input form. Assigns lat/lon to location context
 const PotholeLocation = (prop) => {
   const [location, setLocation] = useState<string>(''); //stores address from input form
-  const { coordinates, setCoordinates } = useContext(LocationContext) //set coordinates using AddPothole LocationContext
+  const { coordinates, setCoordinates } = useContext(LocationContext); //set coordinates using AddPothole LocationContext
   const [pothole_id, setPothole_id] = useState<number>(0);
   const [renderMap, setRenderMap] = useState<boolean>(false);
   const isMounted = useRef(false);
-  const { setSubmissionStatus } = prop; 
+  const { setSubmissionStatus } = prop;
 
   //turns address into lat and lon coordinates
   const updateLatLon = () => {
     const formattedLocation = location.split(' ').join('%20'); //format location to be read by mapbox
-    axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${formattedLocation}.json?language=en&limit=5&proximity=-121.90662,37.42827&country=US&access_token=${mapToken}`) //geo-code
-      .then(
-        ({ data }) => { //set the coordinates 
-          const newCoordinates = { ...coordinates };
-          newCoordinates.lat = data.features[0].center[1];
-          newCoordinates.lon = data.features[0].center[0];
-          setCoordinates(newCoordinates);
-        })
+    axios(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${formattedLocation}.json?language=en&limit=5&proximity=-121.90662,37.42827&country=US&access_token=${mapToken}`
+    )
+      .then(({ data }) => {
+        //set the coordinates
+        const newCoordinates = { ...coordinates };
+        newCoordinates.lat = data.features[0].center[1];
+        newCoordinates.lon = data.features[0].center[0];
+        setCoordinates(newCoordinates);
+      })
       .catch((err) => console.error('FAILURE TO TURN ADDRESS INTO COORDINATES', err));
   };
 
+  //when coordinates change - find the id and set up render of map
   useEffect(() => {
     axios
       .post('/api/pothole/findPothole', { lat: coordinates.lat, lon: coordinates.lon })
@@ -40,48 +43,44 @@ const PotholeLocation = (prop) => {
         const potholeId = data.length > 0 ? data[0].pothole_id : 0;
         setPothole_id(potholeId);
       })
-      .catch(err => console.error('FAILURE TO FINDPOTHOLEID', err))
+      .catch((err) => console.error('FAILURE TO FINDPOTHOLEID', err));
     isMounted.current ? setRenderMap(true) : (isMounted.current = true);
-    }, [coordinates]);
+  }, [coordinates]);
 
-  //makes sure the map renders with the pothole id 
-  // useEffect(() => {
-  //   isMounted.current ? setRenderMap(true) : isMounted.current = true;
-  // }, [coordinates])
-
-  // useEffect(() => {
-  //           pothole_id === 0 ? setSubmissionStatus('notInDB') : setSubmissionStatus('inDB');
-
-  // })
-  
+  //function to render the map once the coordinates changed and map has been rendered
   const handleMapRender = () => {
     if (renderMap === true) {
-      return <PotholePlot coordinates={coordinates} pothole_id={pothole_id} />
+      return <PotholePlot coordinates={coordinates} pothole_id={pothole_id} />;
     } else {
       return null;
     }
-  }
+  };
+
+  //set status of whether the pothole is already in db/not to render appropriate text in location section
+  useEffect(() => {
+    pothole_id === 0 ? setSubmissionStatus('notInDB') : setSubmissionStatus('inDB');
+  });
 
   return (
     <Form.Group className='mb-3'>
       <Form.Group>
-      <InputGroup id='addPotLocation'>
-        <AddressAutofill accessToken={mapToken} browserAutofillEnabled={true}>
-          <Form.Control
-            id='mapfill'
-            name='address'
-            placeholder='Address'
-            type='text'
-            autoComplete='street-address'
-            onChange={(e) => {
-              setLocation(e.target.value);
-            }}
-          />
-        </AddressAutofill>
-      </InputGroup>
+        <InputGroup id='addPotLocation'>
+          <AddressAutofill accessToken={mapToken} browserAutofillEnabled={true}>
+            <Form.Control
+              id='mapfill'
+              name='address'
+              placeholder='Address'
+              type='text'
+              autoComplete='street-address'
+              onChange={(e) => {
+                setLocation(e.target.value);
+              }}
+            />
+          </AddressAutofill>
+        </InputGroup>
         <Button
           variant='flat'
-          onClick={() => {updateLatLon()}}
+          onClick={() => updateLatLon()} //on click coordinates are determined 
         >
           Confirm Pothole Address
         </Button>

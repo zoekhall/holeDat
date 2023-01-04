c90e94c7a3ae488e154b6234b28eb2e57a719c91
codingMustache <jorcar1986@gmail.com>
2023-01-04 09:07:52 -0600

    test users model

--- test/user.model.test.ts
+++ test/user.model.test.ts
@@ -1,11 +1,14 @@
 import {
   getUserData,
-
+  getRecentUsers,
 } from "../server/models/user.model";
 
+test("getUserData returns an Object", ()=>
+getUserData(9000, data=>
+  expect(typeof data).toBe('object'))
+)
 
-test('Am I getting an object', ()=>{
-  getUserData(1, data=>
-     expect(typeof data).toBe('object'))
-})
-
+test("getRecentUsers is returns an Array", ()=>
+getRecentUsers(data=>
+  expect(Array.isArray(data)).toBe(true))
+)

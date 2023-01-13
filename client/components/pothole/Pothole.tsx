d0868d32ef15b5b512d52fa984a9d2abd6ea219c
zoekhall <zoe.k.hall@gmail.com>
2023-01-12 18:48:06 -0600

    rating clean up and caption

--- client/components/pothole/Pothole.tsx
+++ client/components/pothole/Pothole.tsx
@@ -4,6 +4,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
 import { useLocation, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 // import CommentForm from './CommentForm';
 import Likes from './Likes';
 // import Switch from 'react-bootstrap/Switch';
@@ -106,7 +108,6 @@ const Pothole = () => {
 
   return (
     <div id='potholeProfile'>
-
       <Container id='potholeSect' className='post'>
         <Container className='post_header'>
           <div id='address/rating'>
@@ -133,53 +134,60 @@ const Pothole = () => {
             return (
               <SwiperSlide key={image.image_id}>
                 <img className='potHole_img' src={image.photoURL} alt='test' />
-                <div className='post_caption'>
-                  <Link to={'/User:' + image.userId}>
-                    <img
-                      className='avatar'
-                      alt='avatar2'
-                      src={image.userPhoto}
-                    />
-                  </Link>
-                  <div id='innerProfCapt'>
-                  <h3>{image.userName}</h3>
-                  <h4>{image.caption}</h4>
-                  </div>
-                  {user?.name && <Likes user={user} image={image} />}
-                </div>
+                <Container className='post_caption'>
+                  <Row>
+                    <Col>
+                      <Row>
+                        <Col xs={1} id='profBadge'>
+                          <div></div>
+                        </Col>
+                        <Col xs={2} id='profAvatar'>
+                          <Link to={'/User:' + image.userId}>
+                            <img className='avatar' alt='avatar2' src={image.userPhoto} />
+                          </Link>
+                        </Col>
+                        <Col id='innerProfCapt'>
+                          <div>
+                            <h3>{image.userName}</h3>
+                            <h4>{image.caption}</h4>
+                          </div>
+                        </Col>
+                      </Row>
+                    </Col>
+                    <Col xs={2}>{user?.name && <Likes user={user} image={image} />}</Col>
+                  </Row>
+                </Container>
               </SwiperSlide>
             );
           })}
         </Swiper>
-
       </Container>
 
       {/* <CommentForm phId={phId} /> */}
-      
-      </div>
+    </div>
   );
 };
 
 export default Pothole;
 
-          // <div className='rate-cones'>
-          //   <Link to={'/Rating:' + id} state={{ addy, user }}>
-          //     {user?.userId_user && <button>Rate</button>}
-          //   </Link>
-          //   <div className='voting-cones'>
-          //     {Array.from(Array(5)).map((e, i) => {
-          //       return (
-          //         <svg
-          //           key={i}
-          //           xmlns='http://www.w3.org/2000/svg'
-          //           id={`cone-num-${i}`}
-          //           className={`${i < avg ? 'clickCone' : ''}`}
-          //           viewBox='0 0 16 16'
-          //         >
-          //           <path d='m9.97 4.88.953 3.811C10.159 8.878 9.14 9 8 9c-1.14 0-2.158-.122-2.923-.309L6.03 4.88C6.635 4.957 7.3 5 8 5s1.365-.043 1.97-.12zm-.245-.978L8.97.88C8.718-.13 7.282-.13 7.03.88L6.275 3.9C6.8 3.965 7.382 4 8 4c.618 0 1.2-.036 1.725-.098zm4.396 8.613a.5.5 0 0 1 .037.96l-6 2a.5.5 0 0 1-.316 0l-6-2a.5.5 0 0 1 .037-.96l2.391-.598.565-2.257c.862.212 1.964.339 3.165.339s2.303-.127 3.165-.339l.565 2.257 2.391.598z' />
-          //         </svg>
-          //       );
-          //     })}
-          //     {voteCount}
-          //   </div>
-          // </div>
+// <div className='rate-cones'>
+//   <Link to={'/Rating:' + id} state={{ addy, user }}>
+//     {user?.userId_user && <button>Rate</button>}
+//   </Link>
+//   <div className='voting-cones'>
+//     {Array.from(Array(5)).map((e, i) => {
+//       return (
+//         <svg
+//           key={i}
+//           xmlns='http://www.w3.org/2000/svg'
+//           id={`cone-num-${i}`}
+//           className={`${i < avg ? 'clickCone' : ''}`}
+//           viewBox='0 0 16 16'
+//         >
+//           <path d='m9.97 4.88.953 3.811C10.159 8.878 9.14 9 8 9c-1.14 0-2.158-.122-2.923-.309L6.03 4.88C6.635 4.957 7.3 5 8 5s1.365-.043 1.97-.12zm-.245-.978L8.97.88C8.718-.13 7.282-.13 7.03.88L6.275 3.9C6.8 3.965 7.382 4 8 4c.618 0 1.2-.036 1.725-.098zm4.396 8.613a.5.5 0 0 1 .037.96l-6 2a.5.5 0 0 1-.316 0l-6-2a.5.5 0 0 1 .037-.96l2.391-.598.565-2.257c.862.212 1.964.339 3.165.339s2.303-.127 3.165-.339l.565 2.257 2.391.598z' />
+//         </svg>
+//       );
+//     })}
+//     {voteCount}
+//   </div>
+// </div>

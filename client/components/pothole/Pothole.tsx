461a8b8aabc8ff0336cff836f1975831c23c4937
zoekhall <zoe.k.hall@gmail.com>
2023-01-13 13:46:34 -0600

    address looking okay

--- client/components/pothole/Pothole.tsx
+++ client/components/pothole/Pothole.tsx
@@ -8,8 +8,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 // import CommentForm from './CommentForm';
 import Likes from './Likes';
-// import Switch from 'react-bootstrap/Switch';
-// import PotholeRating from '../addPothole/formQuestions/PotholeRating';
+import PotholeRating from '../addPothole/formQuestions/PotholeRating';
+import Switch from 'react-bootstrap/Switch';
 // import PotholeStatus from '../addPothole/formQuestions/PotholeStatus';
 
 const Pothole = () => {
@@ -53,7 +53,7 @@ const Pothole = () => {
     badge_id: undefined,
   });
   const [avg, setAvg] = useState<number>(0);
-  // const [fixed, setFixed] = useState<boolean>(false);
+  const [fixed, setFixed] = useState<boolean>(false);
   // const [voteCount, setVotecount] = useState<number>(0);
 
   const getAllRatingByPhId = () => {
@@ -137,18 +137,31 @@ const Pothole = () => {
     <div id='potholeProfile'>
       <Container id='potholeSect' className='post'>
         <Container className='post_header'>
-          <div id='address/rating'>
+          <div id='address'>
             <h3>{addy}</h3>
-            <div id='score'>{avg}</div>
+            <div id='score'>
+              {avg}
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='25'
+                fill='currentColor'
+                className={`bi bi-cone-striped${'clickCone'}`}
+                viewBox='0 0 16 16'
+              >
+                <path d='m9.97 4.88.953 3.811C10.159 8.878 9.14 9 8 9c-1.14 0-2.158-.122-2.923-.309L6.03 4.88C6.635 4.957 7.3 5 8 5s1.365-.043 1.97-.12zm-.245-.978L8.97.88C8.718-.13 7.282-.13 7.03.88L6.275 3.9C6.8 3.965 7.382 4 8 4c.618 0 1.2-.036 1.725-.098zm4.396 8.613a.5.5 0 0 1 .037.96l-6 2a.5.5 0 0 1-.316 0l-6-2a.5.5 0 0 1 .037-.96l2.391-.598.565-2.257c.862.212 1.964.339 3.165.339s2.303-.127 3.165-.339l.565 2.257 2.391.598z' />
+              </svg>
+            </div>
+          </div>
+
+          <div id='rating_status'>
+          <PotholeRating />
+          </div>
+          
+          <div className='fixed'>
+            <Switch checked={fixed} onChange={() => setFixed(!fixed)} />
+            <p className='xsmall'>Not Fixed</p>
           </div>
         </Container>
-        {/* <div>
-          <PotholeRating />
-          <div className='fixed'>
-          <Switch checked={fixed} onChange={() => setFixed(!fixed)} />
-          <p>Fixed</p>
-          </div>
-        </div> */}
         <Swiper
           className='mySwiper potholeSlider'
           pagination={true}
@@ -166,32 +179,31 @@ const Pothole = () => {
                     <Col>
                       <Row>
                         <Col xs={3} className='badgeAvatar'>
-                        {image?.badge_id && (
-                          <div id='profBadge'>
-                            {badge.map((badges) => {
-                              if (image.badge_id === badges.badge_id) {
-                                return (
-                                  <img
-                                    className='badgeBoy'
-                                    key={badges.badge_id}
-                                    src={badges.imgUrl}
-                                  />
-                                );
-                              }
-                            })}
-                          </div>
-                        )}
-
-                        <div id='profAvatar'>
-                          <Link to={'/User:' + image.userId}>
-                            <img className='avatar' alt='avatar2' src={image.userPhoto} />
-                          </Link>
+                          {image?.badge_id && (
+                            <div id='profBadge'>
+                              {badge.map((badges) => {
+                                if (image.badge_id === badges.badge_id) {
+                                  return (
+                                    <img
+                                      className='badgeBoy'
+                                      key={badges.badge_id}
+                                      src={badges.imgUrl}
+                                    />
+                                  );
+                                }
+                              })}
+                            </div>
+                          )}
+                          <div id='profAvatar'>
+                            <Link to={'/User:' + image.userId}>
+                              <img className='avatar' alt='avatar2' src={image.userPhoto} />
+                            </Link>
                           </div>
                         </Col>
 
                         <Col id='innerProfCapt'>
-                            <h3>{image.userName}</h3>
-                            <h4>{image.caption}</h4>
+                          <h3>{image.userName}</h3>
+                          <h4>{image.caption}</h4>
                         </Col>
                       </Row>
                     </Col>
@@ -232,4 +244,3 @@ export default Pothole;
 //     {voteCount}
 //   </div>
 // </div>
-

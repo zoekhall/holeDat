e3419db8465316a535e6d31e28e235d83a888339
zoekhall <zoe.k.hall@gmail.com>
2023-01-16 15:42:06 -0600

    separated slider from pothole

--- client/components/pothole/Pothole.tsx
+++ client/components/pothole/Pothole.tsx
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
-import { useLocation, Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import CommentForm from './CommentForm';
-import Likes from './Likes';
 import PotholeRating from '../addPothole/formQuestions/PotholeRating';
 import Switch from 'react-bootstrap/Switch';
+import Slider from './Slider';
 // import PotholeStatus from '../addPothole/formQuestions/PotholeStatus';
 
 const Pothole = () => {
@@ -162,58 +158,7 @@ const Pothole = () => {
             <p className='xsmall'>Not Fixed</p>
           </div>
         </Container>
-        <Swiper
-          className='mySwiper potholeSlider'
-          pagination={true}
-          effect={'cards'}
-          id='pothole-profile-slider'
-          grabCursor={true}
-          modules={[Pagination]}
-        >
-          {PImages.map((image) => {
-            return (
-              <SwiperSlide key={image.image_id}>
-                <img className='potHole_img' src={image.photoURL} alt='test' />
-                <Container id='post_caption'>
-                  <Row>
-                    <Col>
-                      <Row>
-                        <Col xs={3} className='badgeAvatar'>
-                          {image?.badge_id && (
-                            <div id='profBadge'>
-                              {badge.map((badges) => {
-                                if (image.badge_id === badges.badge_id) {
-                                  return (
-                                    <img
-                                      className='badgeBoy'
-                                      key={badges.badge_id}
-                                      src={badges.imgUrl}
-                                    />
-                                  );
-                                }
-                              })}
-                            </div>
-                          )}
-                          <div id='profAvatar'>
-                            <Link to={'/User:' + image.userId}>
-                              <img className='avatar' alt='avatar2' src={image.userPhoto} />
-                            </Link>
-                          </div>
-                        </Col>
-
-                        <Col id='innerProfCapt'>
-                          <h3>{image.userName}</h3>
-                          <h4>{image.caption}</h4>
-                        </Col>
-                      </Row>
-                    </Col>
-                    <Col>{user?.name && <Likes user={user} image={image} />}</Col>
-                  </Row>
-                </Container>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
+        <Slider badge={badge} PImages={PImages} user={user} />
       </Container>
       <Container className='comment-container'>
         <CommentForm phId={phId} />
@@ -223,25 +168,3 @@ const Pothole = () => {
 };
 
 export default Pothole;
-
-// <div className='rate-cones'>
-//   <Link to={'/Rating:' + id} state={{ addy, user }}>
-//     {user?.userId_user && <button>Rate</button>}
-//   </Link>
-//   <div className='voting-cones'>
-//     {Array.from(Array(5)).map((e, i) => {
-//       return (
-//         <svg
-//           key={i}
-//           xmlns='http://www.w3.org/2000/svg'
-//           id={`cone-num-${i}`}
-//           className={`${i < avg ? 'clickCone' : ''}`}
-//           viewBox='0 0 16 16'
-//         >
-//           <path d='m9.97 4.88.953 3.811C10.159 8.878 9.14 9 8 9c-1.14 0-2.158-.122-2.923-.309L6.03 4.88C6.635 4.957 7.3 5 8 5s1.365-.043 1.97-.12zm-.245-.978L8.97.88C8.718-.13 7.282-.13 7.03.88L6.275 3.9C6.8 3.965 7.382 4 8 4c.618 0 1.2-.036 1.725-.098zm4.396 8.613a.5.5 0 0 1 .037.96l-6 2a.5.5 0 0 1-.316 0l-6-2a.5.5 0 0 1 .037-.96l2.391-.598.565-2.257c.862.212 1.964.339 3.165.339s2.303-.127 3.165-.339l.565 2.257 2.391.598z' />
-//         </svg>
-//       );
-//     })}
-//     {voteCount}
-//   </div>
-// </div>
---
+++ client/components/pothole/Slider.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Likes from './Likes';
+import { Link } from 'react-router-dom';
+
+
+
+const Slider = (prop) => {
+  const { badge, PImages, user } = prop;
+
+  return  (<Swiper
+      className='mySwiper potholeSlider'
+      pagination={true}
+      effect={'cards'}
+      id='pothole-profile-slider'
+      grabCursor={true}
+      modules={[Pagination]}
+    >
+      {PImages.map((image) => {
+        return (
+          <SwiperSlide key={image.image_id}>
+            <img className='potHole_img' src={image.photoURL} alt='test' />
+            <Container id='post_caption'>
+              <Row>
+                <Col>
+                  <Row>
+                    <Col xs={3} className='badgeAvatar'>
+                      {image?.badge_id && (
+                        <div id='profBadge'>
+                          {badge.map((badges) => {
+                            if (image.badge_id === badges.badge_id) {
+                              return (
+                                <img
+                                  className='badgeBoy'
+                                  key={badges.badge_id}
+                                  src={badges.imgUrl}
+                                />
+                              );
+                            }
+                          })}
+                        </div>
+                      )}
+                      <div id='profAvatar'>
+                        <Link to={'/User:' + image.userId}>
+                          <img className='avatar' alt='avatar2' src={image.userPhoto} />
+                        </Link>
+                      </div>
+                    </Col>
+
+                    <Col id='innerProfCapt'>
+                      <h3>{image.userName}</h3>
+                      <h4>{image.caption}</h4>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col>{user?.name && <Likes user={user} image={image} />}</Col>
+              </Row>
+            </Container>
+          </SwiperSlide>
+        );
+      })}
+  </Swiper>
+)
+}
+
+export default Slider;

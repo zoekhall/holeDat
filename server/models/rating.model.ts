d119d80ceca74548bd767ad298acb154731f35fe
zoekhall <zoe.k.hall@gmail.com>
2023-01-24 00:27:02 -0600

    profile rating updates on new rating

--- server/models/rating.model.ts
+++ server/models/rating.model.ts
@@ -24,20 +24,36 @@ export const addRating = (pothole_id: number, user_id: number, fixed: boolean, r
   Rating.findOne({ where: { pothole_id, user_id } }).then((data) => {
     if (!data) {
       Rating.create({ pothole_id, fixed, overall: rating, user_id })
-        // .then(data => console.log(data))
-        .catch((err) =>
-        console.log(err)
+        .catch((err) => console.log(err)
       );
     } else {
       Rating.update({ fixed, overall: rating }, { where: { pothole_id, user_id } })
-        // .then((data) => console.log(data, pothole_id, rating, user_id))
+
         .catch((err) => console.log(err));
     }
-  });
+  })
 };
 
 export const getAllRatingsById = async (id, cb) => {
   Rating.findAll({ where: { pothole_id: id } })
     .then((data) => cb(data))
     .catch((err) => console.error(err));
+};
+
+export const addRatingAndFind = (pothole_id: number, user_id: number, fixed: boolean, rating: number, cb) => {
+  Rating.findOne({ where: { pothole_id, user_id } })
+    .then((data) => {
+      if (!data) {
+        Rating.create({ pothole_id, fixed, overall: rating, user_id })
+          .catch((err) => console.log(err));
+      } else {
+        Rating.update({ fixed, overall: rating }, { where: { pothole_id, user_id } })
+          .catch((err) => console.log(err));
+      }
+    })
+    .then(() => {
+      Rating.findAll({ where: { pothole_id: pothole_id } })
+        .then((data) => cb(data))
+        .catch((err) => console.error(err));
+    });
 };
